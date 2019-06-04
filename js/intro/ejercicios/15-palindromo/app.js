0e5377fda70d82676ce280fb72903f848beb7152
Dadelato <dadelato@hotmail.com>
2019-06-04 17:17:31 +0200

    Strings

---
+++ js/intro/ejercicios/15-palindromo/app.js
@@ -0,0 +1,25 @@
+function esPalindromo(texto){
+    var palindromo = texto.replace(/ /g,"");
+    //console.log(palindromo);
+    //console.log(palindromo.length);
+    var comprobar = true;
+    for(var i=0; i<palindromo.length; i++){
+        if(palindromo[i] !== palindromo[palindromo.length-i-1]){
+            //console.log(palindromo[i] !== palindromo[palindromo.length-i-1]);
+            comprobar = false;
+        }
+    }
+    return comprobar;
+}
+console.log(esPalindromo("ha  h"));
+
+/*
+function esPalindro(texto){
+    var textoSinEspacios = texto.replace(/ /g,"");
+    var arrayLetras = textoSinEspacios.split("");
+    var arrayAlReves = arrayLetras.reverse();
+    var textoAlReves = arrayAlReves.join("");
+
+    return textoSinEspacios === textoAlReves;
+}
+*/
--- js/intro/objetos.js
+++ js/intro/objetos.js
@@ -54,3 +54,49 @@ console.log(twd);
 
 var cancionBatman = new Array(16).join("what"-1) + " Batman!";
 console.log(cancionBatman);
+
+//Ejercicio 14
+var numeros = [1, 34, 12, 64, 75, 343, 64, 75, 2323, 54, 6];
+
+var ej14 = numeros.map(function(num){
+    return num % 2 === 0 ? "Par" : num;
+});
+
+console.log(ej14);
+
+//Strings
+var texto = "Hola mundo!";
+console.log(texto.length);
+console.log(texto.toUpperCase());
+console.log(texto.replace("o",0));
+console.log(texto.replace(/o/gi,0));
+console.log(texto.substring(3, 6));
+console.log(texto.indexOf("!"));
+console.log(texto.includes("o"));
+console.log(texto.split(""));
+
+
+
+var colores = ["amarillo", "rojo", "verde"];
+var primero = colores.shift();
+var ultimo = colores.pop();
+console.log(primero, ultimo);
+colores.push("blanco");
+colores.unshift("azul");
+colores = colores.concat([primero, ultimo]);
+console.log(colores);
+
+
+var hoy = new Date();
+console.log(hoy.getDay());
+console.log(hoy.getDate());
+console.log(typeof(hoy));
+
+var angelJSON = JSON.stringify({nombre: "Angel"});
+//console.log(angel);
+/*
+var angelJS = JSON.parse(angelJSON);
+console.log(angel);
+console.log(angelJSON);
+console.log(angelJS);
+*/
